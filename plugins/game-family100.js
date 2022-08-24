99c535a10b8439e5e87a275e08e8d1a52c0fe974
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-08-25 01:20:21 +0700

    Update game-family100.js

--- plugins/game-family100.js
+++ plugins/game-family100.js
@@ -1,5 +1,5 @@
-let fetch = require('node-fetch')
-let winScore = 500
+import { family100 } from '@bochilteam/scraper'
+const winScore = 4999
 async function handler(m) {
     this.game = this.game ? this.game : {}
     let id = 'family100_' + m.chat
@@ -7,22 +7,17 @@ async function handler(m) {
         this.reply(m.chat, 'Masih ada kuis yang belum terjawab di chat ini', this.game[id].msg)
         throw false
     }
-    let res = await fetch(global.API('xteam', '/game/family100', {}, 'APIKEY'))
-    if (!res.ok) throw await res.text()
-    let json = await res.json()
-    if (!json.status) throw json
+    const json = await family100()
     let caption = `
 *Soal:* ${json.soal}
-
 Terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' ')) ? `
 (beberapa jawaban terdapat spasi)
 `: ''}
-
 +${winScore} XP tiap jawaban benar
     `.trim()
     this.game[id] = {
         id,
-        msg: await m.reply(caption),
+        msg: await this.sendButton(m.chat, caption, author, null, [['Nyerah', 'nyerah']], m),
         ...json,
         terjawab: Array.from(json.jawaban, () => false),
         winScore,
@@ -32,4 +27,4 @@ handler.help = ['family100']
 handler.tags = ['game']
 handler.command = /^family100$/i
 
-module.exports = handler
+export default handler
